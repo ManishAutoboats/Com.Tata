7851e0f0b5e402554ea5aa6f2e9023a38cb375b8
Manish <manishtesqaat@gmail.com>
2024-07-02 22:18:24 +0530

    dfsf

---
+++ src/main/java/com/helper/Test.java
@@ -0,0 +1,23 @@
+package com.helper;
+
+public class Test {
+	
+	private int account;
+	private String name;
+	
+	public int getAccount() {
+		return account;
+	}
+	public void setAccount(int account) {
+		this.account = account;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/helper/demo.java
@@ -0,0 +1,13 @@
+package com.helper;
+
+public class demo {
+	
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		t.setAccount(90876);
+		System.out.println(t.getAccount());
+		
+		}
+
+}
